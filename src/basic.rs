596e4632ad3e097dfa693d06fbae33ab4ce55509
Fabian Zaiser <fabian.zaiser@gmail.com>
2015-11-21 16:31:16 -0500

    Add a little documentation on functions testing for intersections.

--- src/basic.rs
+++ src/basic.rs
@@ -78,10 +78,16 @@ impl Intersection {
 }
 
 /// Trait for finding ray intersections.
+/// Instances **must** satisfy the law:
+///
+/// ```text
+/// s.is_hit_by(ray, tmax) == s.intersect(ray, tmax).is_some()
+/// ```
 pub trait Surface {
     /// Returns information about the intersection of the object and the ray, if one exists.
     /// If the distance is greater that `t_max`, it returns `None`.
     fn intersect(&self, ray: Ray, t_max: f64) -> Option<Intersection>;
+
     /// Checks whether the ray intersects the object, computes no additional information.
     /// If the distance is greater than `t_max`, it returns `false`.
     fn is_hit_by(&self, ray: Ray, t_max: f64) -> bool;
